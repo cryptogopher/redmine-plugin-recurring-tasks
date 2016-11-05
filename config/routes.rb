b335564dc06bab4d49af8b7a878d84e1bff00e90
Piotr Michalczyk <github@michalczyk.pro>
2016-11-05 19:20:11 +0000

    Updated for Redmine 3.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # resources :recurring_tasks
 
 # admin view
-match 'recurring_tasks', :to => 'recurring_tasks#index'
+get 'recurring_tasks', :to => 'recurring_tasks#index'
 
 # project view 
-match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index', :as => :recurring_tasks
-match 'projects/:project_id/recurring_tasks/new(/:issue_id)', :to => 'recurring_tasks#new', :as => :new_recurring_task
-match 'projects/:project_id/recurring_tasks/create', :to => 'recurring_tasks#create'
-match 'projects/:project_id/recurring_tasks/:id/edit', :to => 'recurring_tasks#edit', :as => :edit_recurring_task
-match 'projects/:project_id/recurring_tasks/:id/update', :to => 'recurring_tasks#update'
-match 'projects/:project_id/recurring_tasks/:id/destroy', :to => 'recurring_tasks#destroy', :as => :destroy_recurring_task
+#get 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index', :as => :recurring_tasks
+get 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
+match 'projects/:project_id/recurring_tasks/new(/:issue_id)', :to => 'recurring_tasks#new', :as => :new_recurring_task, via: [:get, :post]
+match 'projects/:project_id/recurring_tasks/create', :to => 'recurring_tasks#create', via: [:get, :post]
+match 'projects/:project_id/recurring_tasks/:id/edit', :to => 'recurring_tasks#edit', :as => :edit_recurring_task, via: [:get, :post]
+match 'projects/:project_id/recurring_tasks/:id/update', :to => 'recurring_tasks#update', via: [:get, :post]
+match 'projects/:project_id/recurring_tasks/:id/destroy', :to => 'recurring_tasks#destroy', :as => :destroy_recurring_task, via: [:get, :post]
